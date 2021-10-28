6f40d0775ce89e204d8a7fd13b60c2de926a110c
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-10-28 07:50:00 -0700

    tub sonlar

---
+++ main.js
@@ -0,0 +1,15 @@
+
+//  1 dan 100 gacha sonlar
+// for (let i = 0; i <= 100; i++)
+//  {
+//      	console.log(i);
+
+// }
+
+
+//  1 dan 100 gacha sonlar  teskasri tartibda
+for (let i =0; i <100; i++)
+ {
+     	console.log(i);
+
+}
